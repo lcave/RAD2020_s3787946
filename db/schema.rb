d68f47395ab924007a15b6db55bac1af47246a5c
[luca cave s3787946] <[s3787946@student.rmit.edu.au]>
2020-04-28 15:33:06 +1000

    Implemented remembering users using cookies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200419120200) do
+ActiveRecord::Schema.define(version: 20200428020609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20200419120200) do
     t.string "password"
     t.string "password_confirmation"
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
   add_foreign_key "microposts", "users"
