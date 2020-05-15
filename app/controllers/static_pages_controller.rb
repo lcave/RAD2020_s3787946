efee3da2ac70cdb7bbb88d42d0bd5f587ac86316
[luca cave s3787946] <[s3787946@student.rmit.edu.au]>
2020-05-15 17:05:53 +1000

    Added recent users to homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
+    @users = User.order(created_at: :asc).last(6)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,21 @@ User.create!(name: "RAD",
              mobile: "0422222222",
              password: "Rails2020",
              password_confirmation: "Rails2020")
+
+User.create!(name: "Foo",
+             email: "foo@bar.org",
+             mobile: "0422222222",
+             password: "password",
+             password_confirmation: "password")
+
+User.create!(name: "Bar",
+             email: "bar@baz.org",
+             mobile: "0422222222",
+             password: "password",
+             password_confirmation: "password")
+
+User.create!(name: "Baz",
+             email: "baz@blu.org",
+             mobile: "0422222222",
+             password: "password",
+             password_confirmation: "password")
