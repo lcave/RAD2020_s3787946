8c5b9de1c852bd313ff900157ffe771aef25b929
[luca cave s3787946] <[s3787946@student.rmit.edu.au]>
2020-05-19 00:15:29 +1000

    Further improved css and db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,23 +4,16 @@ User.create!(name: "RAD",
              password: "Rails2020",
              password_confirmation: "Rails2020")
 
-User.create!(name: "Foo",
-             email: "foo@bar.org",
-             mobile: "0422222222",
-             password: "password",
-             password_confirmation: "password")
-
-User.create!(name: "Bar",
-             email: "bar@baz.org",
-             mobile: "0422222222",
-             password: "password",
-             password_confirmation: "password")
-
-User.create!(name: "Baz",
-             email: "baz@blu.org",
-             mobile: "0422222222",
-             password: "password",
-             password_confirmation: "password")
+99.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email()
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               mobile: "0422222222",
+               password_confirmation: password)
+end
 
 users = []
 
@@ -32,8 +25,8 @@ topics = ["News", "A.I.", "V.R.", "RMIT", "Game", "Rails", "Go", "Web", "M.L", "
 
 for topic in topics
   rand(50).times do |n|
-    Micropost.create!(content: "General Kenobi",
+    Micropost.create!(content: Faker::Lorem.paragraph(sentence_count: rand(20..60)),
                       user_id: users.sample.id,
-                      title: "Hello there", views: rand(1000000), topic: topic)
+                      title: Faker::Lorem.sentence(word_count: rand(2..15)), views: rand(1000000), topic: topic)
   end
 end
