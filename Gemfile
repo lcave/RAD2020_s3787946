d71143c2ef9f7d8300f74ca48cfb47568465a802
[luca cave s3787946] <[s3787946@student.rmit.edu.au]>
2020-05-21 22:46:47 +1000

    Created micropost new page with rich text editing, made minor changes to css and html layout

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'will_paginate'
 gem 'bootstrap-will_paginate'
 gem 'faker'
 gem 'carrierwave'
+gem 'trix'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,15 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :new, :create, :destroy]
+
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      flash[:danger] = "Something went wrong"
+      redirect_to root_url
+    end
   end
 
   def show
@@ -8,10 +18,28 @@ class MicropostsController < ApplicationController
   end
 
   def index
-    @articles = Article.all
   end
 
   def new
-    @article = Article.new
+    @post = Micropost.new
+  end
+
+  def destroy
+    @micropost.destroyflash[:success] = "Micropost deleted"
+    redirect_to root_path
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:title, :topic, :content)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 end
